be2f643f3241900238a772f5a4587d3185c95f61
siri906 <sirirang906@gmail.com>
2024-07-21 21:58:41 +0900

    임의의 기본 값 설정하는 방법

--- app/list/page.tsx
+++ app/list/page.tsx
@@ -4,13 +4,13 @@ export default function Page() {
       <div className="bg-white w-full shadow-lg p-5 rounded-3xl max-w-screen-sm flex flex-col gap-2 *:outline-none has-[.peer]:bg-green-300">
         {["test1", "test2", "test3", "test4"].map((person, index) => {
           return (
-            <div key={index} className="flex items-center gap-3">
+            <div key={index} className="flex items-center gap-3 group">
               <div className="flex items-center size-10 bg-blue-400 rounded-full"></div>
-              <span className="text-lg font-medium">{person}</span>
+              <span className="text-lg font-medium group-hover:text-red-400">{person}</span>
               {/* <div className="animate-bounce size-6 bg-red-500 text-white flex items-center justify-center rounded-full">
                 <span>{index}</span>
               </div> */}
-              <div className="relative size-6 bg-red-500 text-white flex items-center justify-center rounded-full">
+              <div className="relative size-6 bg-red-500 text-white flex items-center justify-center  rounded-sexy-name">
                 <span className="z-10">{index}</span>
                 <span className="animate-ping size-6 bg-red-500 text-white flex items-center justify-center rounded-full absolute"></span>
               </div>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,7 +2,13 @@ import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: ["./pages/**/*.{js,ts,jsx,tsx,mdx}", "./components/**/*.{js,ts,jsx,tsx,mdx}", "./app/**/*.{js,ts,jsx,tsx,mdx}"],
-  theme: {},
+  theme: {
+    extend: {
+      borderRadius: {
+        "sexy-name": "11.11px",
+      },
+    },
+  },
   plugins: [],
 };
 export default config;
